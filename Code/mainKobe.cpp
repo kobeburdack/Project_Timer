bc9d1b0a7ddae52de94cf562751555e6321403c8
kobeburdack <44900554+kobeburdack@users.noreply.github.com>
2018-11-09 16:34:38 +0100

    v1

--- Code/mainKobe.cpp
+++ Code/mainKobe.cpp
@@ -1,5 +1,56 @@
 #include <Arduino.h>
 
+
+class Data
+{
+private:
+
+int penaltyTimeLenght = 6000;
+int startTime1;
+int startTime2;
+int time2;
+int time2;
+int pauseTijd;
+int totalTime;
+bool stateCounter1 = false;
+bool stateCounter2 = false;
+bool statePause = false;
+
+
+public:
+
+String name;
+int number = 0;
+
+startCounter1(){
+  startTime1 = millis();
+  stateCounter1 = true;
+}
+stopCounter1(){
+  time2 = millis() - startTime1;
+  stateCounter1 = false;
+}
+
+startCounter2(){
+  startTime1 = millis();
+  stateCounter2 = true;
+}
+stopCounter2(){
+  time2 = millis() - startTime1;
+  stateCounter2 = false;
+}
+pauzeTimer(){
+  if(startTime1 == true){
+    int time = millis() - startTime1;
+    
+  }
+}
+
+}
+
+
+
+
 void setup() {
   Serial.begin(9600);
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,11 @@
 #include <Arduino.h>
 
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(9600);
+  Serial.println("Dit werkt!");
 }
+
+
 
 void loop() {
   // put your main code here, to run repeatedly:
